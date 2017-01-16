3e9ce6034b032f7367e42d51ed9c3f2255a640ac
Brett Macy <bam03584@aol.com>
2017-01-15 20:12:33 -0500

    styling updates

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const myApp = angular.module('myApp', []);
 myApp.controller('ListCtrl', function($scope, $http){
   $scope.title = "Angular One List";
   $scope.todos = [];
+  $scope.complete = false;
 
   $scope.updateName = () => {
     if (event.keyCode === 13) {
-      //$scope.todos.push({'name': event.target.value});
       $http({
         method:'POST',
         url: 'https://one-list-api.herokuapp.com/items?access_token=harley',
@@ -16,26 +16,37 @@ myApp.controller('ListCtrl', function($scope, $http){
             text: event.target.value
           }
         })
-      }).then((response) => $scope.todos.push(response.data))
-        event.target.value = '';
+      })
+      .then((response) => $scope.todos.push(response.data))
+      event.target.value = '';
       }
     };
 
     $scope.deleteItem = (index, todo) => {
-      console.log('DELETE', index, todo);
       $http({
         method: 'DELETE',
         url: `https://one-list-api.herokuapp.com/items/${todo.id}?access_token=harley`,
         headers: { 'Content-Type': 'application/json' }
-      }).then(() => $scope.todos.splice(index, 1))
+      })
+      .then(() => $scope.todos.splice(index, 1))
+    }
+
+    $scope.completeTodo = () => {
+      if ($scope.complete) {
+        $scope.complete = false;
+        event.target.style.textDecoration = 'none';
+        event.target.style.opacity = 1;
+      } else {
+        $scope.complete = true;
+        event.target.style.textDecoration = 'line-through';
+        event.target.style.opacity = 0.5;
+      }
     }
 
 
   $http({
-  method: 'GET',
-  url: 'https://one-list-api.herokuapp.com/items?access_token=harley'
-}).then(function successCallback(response) {
-    console.log(response);
-    response.data.forEach(res => $scope.todos.push(res));
+    method: 'GET',
+    url: 'https://one-list-api.herokuapp.com/items?access_token=harley'
   })
+  .then((response) => response.data.forEach(res => $scope.todos.push(res)))
 });
